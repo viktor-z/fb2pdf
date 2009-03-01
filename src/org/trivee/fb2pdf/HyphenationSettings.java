4dd54f065c72db837fb32eebd83f696fc00392a3
viktor-z <viktor-z>
2009-03-01 02:46:51 +0000

    Added hyphenation settings
    git-svn-id: https://fb2pdf.googlecode.com/svn/trunk/src/java@477 f05116cd-5028-0410-9863-11db5debc633

---
+++ src/org/trivee/fb2pdf/HyphenationSettings.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.trivee.fb2pdf;
+
+/**
+ *
+ * @author vzeltser
+ */
+public class HyphenationSettings {
+
+    public String defaultLanguage;
+    public boolean hyphenate;
+    public boolean overrideLanguage;
+
+    public HyphenationSettings()
+    {
+    }
+}
