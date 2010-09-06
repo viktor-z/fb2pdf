ddbad0835b62a6210c186496984e1cc445f6feb7
viktor-z <viktor-z>
2010-09-06 23:23:34 +0000

    Fixed lost metainfo and bookmarks
    git-svn-id: https://fb2pdf.googlecode.com/svn/trunk/src/java@688 f05116cd-5028-0410-9863-11db5debc633

--- src/org/trivee/utils/TwoUp.java
+++ src/org/trivee/utils/TwoUp.java
@@ -9,9 +9,15 @@ import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfImportedPage;
 import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.SimpleBookmark;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.HashMap;
+import java.util.List;
 import org.apache.commons.lang.StringUtils;
 
 /**
@@ -43,27 +49,39 @@ public class TwoUp {
             // step 1: creation of a document-object
             Document document = new Document(newSize, 0, 0, 0, 0);
             // step 2: we create a writer that listens to the document
-            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(dest));
+            ByteArrayOutputStream tmp = new ByteArrayOutputStream();
+            PdfWriter writer = PdfWriter.getInstance(document, tmp);
+            writer.setFullCompression();
             // step 3: we open the document
             document.open();
             // step 4: adding the content
             PdfContentByte cb = writer.getDirectContent();
             PdfImportedPage page;
             float offsetX;
-            int p;
-            for (int i = 0; i < total; i++) {
+            for (int p = 1; p <= total; p++) {
                 offsetX = 0;
-                if (i % 2 == 0) {
+                if (p % 2 != 0) {
                     document.newPage();
                 } else {
                     offsetX = pageSize.getWidth();
                 }
-                p = i + 1;
                 page = writer.getImportedPage(reader, p);
                 cb.addTemplate(page, offsetX, 0);
             }
             // step 5: we close the document
             document.close();
+
+            PdfReader reader2 = new PdfReader(new ByteArrayInputStream(tmp.toByteArray()));
+            PdfStamper stamper = new PdfStamper(reader2, new FileOutputStream(dest));
+            stamper.setMoreInfo(reader.getInfo());
+            List<HashMap<String, Object>> outlines = SimpleBookmark.getBookmark(reader);
+            for (int pageNum=1; pageNum<=total; pageNum++){
+                int[] range = {pageNum,pageNum};
+                int newPageNum = (pageNum / 2) + (pageNum % 2 != 0 ? 1 : 0);
+                SimpleBookmark.shiftPageNumbers(outlines, newPageNum - pageNum, range);
+            }
+            stamper.setOutlines(outlines);
+            stamper.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
