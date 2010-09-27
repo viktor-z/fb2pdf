189c7cd1d827383a2d494ba74283bf1851b38d52
viktor-z <viktor-z>
2010-09-27 07:51:25 +0000

    Footnotes under construction
    git-svn-id: https://fb2pdf.googlecode.com/svn/trunk/src/java@695 f05116cd-5028-0410-9863-11db5debc633

--- src/org/trivee/fb2pdf/FootnoteRenderer.java
+++ src/org/trivee/fb2pdf/FootnoteRenderer.java
@@ -4,27 +4,15 @@
  */
 package org.trivee.fb2pdf;
 
+import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Image;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.BaseFont;
-import com.itextpdf.text.pdf.PdfDocument;
-import com.itextpdf.text.pdf.PdfImportedPage;
 import com.itextpdf.text.pdf.PdfPageEventHelper;
-import com.itextpdf.text.pdf.PdfReader;
-import com.itextpdf.text.pdf.PdfStamper;
-import com.itextpdf.text.pdf.PdfTemplate;
 import com.itextpdf.text.pdf.PdfWriter;
 import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -32,27 +20,15 @@ import java.util.logging.Logger;
  */
 public class FootnoteRenderer {
 
-    static List<Image> renderFootnoteImages(String body, Stylesheet stylesheet) throws DocumentException, FB2toPDFException {
-
-        boolean savedPreventWidows = PdfDocument.preventWidows;
-        PdfDocument.preventWidows = false;
-        try {
-            byte[] pdf = renderNoteDoc(stylesheet, body);
-            List<Image> images = getImages(pdf);
-            return images;
-        } finally {
-            PdfDocument.preventWidows = savedPreventWidows;
-        }
-    }
-
     public static byte[] renderNoteDoc(Stylesheet stylesheet, String body) throws FB2toPDFException, DocumentException {
         final PageStyle pageStyle = stylesheet.getPageStyle();
         final ParagraphStyle noteStyle = stylesheet.getParagraphStyle("footnote");
         float w = pageStyle.getPageWidth() - pageStyle.getMarginLeft() - pageStyle.getMarginRight();
         float fontSize = noteStyle.getFontSize().getPoints();
         BaseFont basefont = noteStyle.getBaseFont();
-        float h = basefont.getFontDescriptor(BaseFont.CAPHEIGHT, fontSize) + noteStyle.getAbsoluteLeading();
+        float h = basefont.getFontDescriptor(BaseFont.CAPHEIGHT, fontSize) + noteStyle.getAbsoluteLeading() / 2;
         Rectangle pageSize = new Rectangle(w, h);
+        pageSize.setBackgroundColor(BaseColor.LIGHT_GRAY);
         Document doc = new Document(pageSize, pageStyle.getMarginLeft(), pageStyle.getMarginRight(), pageStyle.getMarginTop(), pageStyle.getMarginBottom());
         PdfWriter writer = null;
         ByteArrayOutputStream output = new ByteArrayOutputStream();
@@ -64,25 +40,6 @@ public class FootnoteRenderer {
         doc.add(p);
         doc.close();
         return output.toByteArray();
-    }
-
-    private static List<Image> getImages(byte[] pdf) {
-
-        List<Image> result = new ArrayList<Image>();
-        try {
-            PdfReader reader = new PdfReader(pdf);
-            PdfStamper stamper = new PdfStamper(reader, new ByteArrayOutputStream());
-
-            for (int i=1; i<reader.getNumberOfPages(); i++){
-                PdfImportedPage page = stamper.getImportedPage(reader, i);
-                Image image = Image.getInstance(page);
-                result.add(image);
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
-        return result;
     }
 
     private static class PageEvents extends PdfPageEventHelper {
