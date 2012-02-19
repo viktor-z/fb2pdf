16b1922244e033ad98064a77e091d32702ff72ff
viktor-z <viktor-z>
2012-02-19 00:51:58 +0000

    GUI
    git-svn-id: https://fb2pdf.googlecode.com/svn/trunk/src/java@961 f05116cd-5028-0410-9863-11db5debc633

--- fb2pdf-GUI.cmd
+++ fb2pdf-GUI.cmd
@@ -1,2 +1,2 @@
 @echo off
-start javaw -cp ./UI;./lib/pivot-core-2.0.1.jar;./lib/pivot-wtk-2.0.1.jar;./lib/pivot-wtk-terra-2.0.1.jar;./lib/fb2pdf.jar;./lib/js-1.7R2.jar org.apache.pivot.wtk.ScriptApplication --src="/main.bxml" --resources="localization"
+start javaw -cp ./UI;./lib/pivot-core-2.0.1.jar;./lib/pivot-wtk-2.0.1.jar;./lib/pivot-wtk-terra-2.0.1.jar;./lib/fb2pdf.jar org.apache.pivot.wtk.ScriptApplication --src="/main.bxml" --resources="localization"
