879a0423ce0d3bacf02e51b481db8c89691ed1fd
viktor-z <viktor-z>
2012-04-29 02:08:16 +0000

    Parameters in UI under construction
    git-svn-id: https://fb2pdf.googlecode.com/svn/trunk/src/java@984 f05116cd-5028-0410-9863-11db5debc633

--- fb2pdf-GUI.cmd
+++ fb2pdf-GUI.cmd
@@ -1,2 +1,3 @@
 @echo off
 start javaw -cp ./UI;./lib/pivot-core-2.0.1.jar;./lib/pivot-wtk-2.0.1.jar;./lib/pivot-wtk-terra-2.0.1.jar;./lib/fb2pdf.jar org.apache.pivot.wtk.ScriptApplication --src="/main.bxml" --resources="localization"
+rem start javaw -Duser.language=ru -cp ./UI;./lib/pivot-core-2.0.1.jar;./lib/pivot-wtk-2.0.1.jar;./lib/pivot-wtk-terra-2.0.1.jar;./lib/fb2pdf.jar org.apache.pivot.wtk.ScriptApplication --src="/main.bxml" --resources="localization"
