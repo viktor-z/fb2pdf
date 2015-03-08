13f43bea549a5568b250edd142be82d1ddb08660
viktor-z <viktor-z>
2015-03-08 01:10:33 +0000

    Fixing UI for Java 8
    git-svn-id: https://fb2pdf.googlecode.com/svn/trunk/src/java@1039 f05116cd-5028-0410-9863-11db5debc633

--- fb2pdf-GUI.cmd
+++ fb2pdf-GUI.cmd
@@ -1,3 +1,3 @@
 @echo off
-start javaw -cp ./UI;./lib/pivot-core-2.0.1.jar;./lib/pivot-wtk-2.0.1.jar;./lib/pivot-wtk-terra-2.0.1.jar;./lib/fb2pdf.jar org.apache.pivot.wtk.ScriptApplication --src="/main.bxml" --resources="localization"
-rem start javaw -Duser.language=ru -cp ./UI;./lib/pivot-core-2.0.1.jar;./lib/pivot-wtk-2.0.1.jar;./lib/pivot-wtk-terra-2.0.1.jar;./lib/fb2pdf.jar org.apache.pivot.wtk.ScriptApplication --src="/main.bxml" --resources="localization"
+start javaw -cp ./UI;./lib/pivot-core-2.0.1.jar;./lib/pivot-wtk-2.0.1.jar;./lib/pivot-wtk-terra-2.0.1.jar;./lib/js.jar;./lib/js-engine.jar;./lib/fb2pdf.jar org.apache.pivot.wtk.ScriptApplication --src="/main.bxml" --resources="localization"
+rem start javaw -Duser.language=ru -cp ./UI;./lib/pivot-core-2.0.1.jar;./lib/pivot-wtk-2.0.1.jar;./lib/pivot-wtk-terra-2.0.1.jar;./lib/js.jar;./lib/js-engine.jar;./lib/fb2pdf.jar org.apache.pivot.wtk.ScriptApplication --src="/main.bxml" --resources="localization"
--- fb2pdf-GUI.sh
+++ fb2pdf-GUI.sh
@@ -1,1 +1,1 @@
-java -cp ./UI:./lib/pivot-core-2.0.1.jar:./lib/pivot-wtk-2.0.1.jar:./lib/pivot-wtk-terra-2.0.1.jar:./lib/fb2pdf.jar org.apache.pivot.wtk.ScriptApplication --src="/main.bxml" --resources="localization"
+java -cp ./UI:./lib/pivot-core-2.0.1.jar:./lib/pivot-wtk-2.0.1.jar:./lib/pivot-wtk-terra-2.0.1.jar:./lib/js.jar:./lib/js-engine.jar:./lib/fb2pdf.jar org.apache.pivot.wtk.ScriptApplication --src="/main.bxml" --resources="localization"
