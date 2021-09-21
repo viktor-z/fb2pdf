77148e230418ecf142106616428476cec9a747b1
rupor-github <rupor0mail@gmail.com>
2021-09-21 10:34:52 -0400

    Maintenance release - updated dependencies, JDK 17

--- src/main/java/com/itextpdf/text/pdf/security/PdfPKCS7.java
+++ src/main/java/com/itextpdf/text/pdf/security/PdfPKCS7.java
@@ -805,7 +805,7 @@ public class PdfPKCS7 {
             // Add the digestAlgorithm
             v = new ASN1EncodableVector();
             v.add(new ASN1ObjectIdentifier(digestAlgorithmOid));
-            v.add(new DERNull());
+            v.add(DERNull.INSTANCE);
             signerinfo.add(new DERSequence(v));
 
             // add the authenticated attribute if present
@@ -815,7 +815,7 @@ public class PdfPKCS7 {
             // Add the digestEncryptionAlgorithm
             v = new ASN1EncodableVector();
             v.add(new ASN1ObjectIdentifier(digestEncryptionAlgorithmOid));
-            v.add(new DERNull());
+            v.add(DERNull.INSTANCE);
             signerinfo.add(new DERSequence(v));
 
             // Add the digest
